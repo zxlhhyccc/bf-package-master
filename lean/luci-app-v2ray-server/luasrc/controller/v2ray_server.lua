eb6d91953d9a01b86a1dc5dc6b50965d9cf7b88c
zxlhhyccc <zxlhhy@gmail.com>
2020-03-02 21:20:26 +0800

    up ssrplus adbyby v2-sever

--- lean/luci-app-v2ray-server/luasrc/controller/v2ray_server.lua
+++ lean/luci-app-v2ray-server/luasrc/controller/v2ray_server.lua
@@ -1,4 +1,3 @@
--- Copyright 2018-2019 Lienol <lawlienol@gmail.com>
 module("luci.controller.v2ray_server", package.seeall)
 local http = require "luci.http"
 local v2ray = require "luci.model.cbi.v2ray_server.api.v2ray"
@@ -13,10 +12,6 @@ function index()
 
     entry({"admin", "vpn", "v2ray_server", "users_status"},
           call("v2ray_users_status")).leaf = true
-    entry({"admin", "vpn", "v2ray_server", "check"}, call("v2ray_check")).leaf =
-        true
-    entry({"admin", "vpn", "v2ray_server", "update"}, call("v2ray_update")).leaf =
-        true
     entry({"admin", "vpn", "v2ray_server", "get_log"}, call("get_log")).leaf =
         true
     entry({"admin", "vpn", "v2ray_server", "clear_log"}, call("clear_log")).leaf =
@@ -35,26 +30,6 @@ function v2ray_users_status()
                    "ps -w| grep -v grep | grep '/var/etc/v2ray_server/" ..
                        luci.http.formvalue("id") .. "' >/dev/null") == 0
     http_write_json(e)
-end
-
-function v2ray_check()
-    local json = v2ray.to_check("")
-    http_write_json(json)
-end
-
-function v2ray_update()
-    local json = nil
-    local task = http.formvalue("task")
-    if task == "extract" then
-        json =
-            v2ray.to_extract(http.formvalue("file"), http.formvalue("subfix"))
-    elseif task == "move" then
-        json = v2ray.to_move(http.formvalue("file"))
-    else
-        json = v2ray.to_download(http.formvalue("url"))
-    end
-
-    http_write_json(json)
 end
 
 function get_log()
