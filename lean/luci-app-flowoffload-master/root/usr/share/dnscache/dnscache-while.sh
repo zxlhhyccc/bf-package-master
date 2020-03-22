8c9f8199e7016a6c4c9b7f333e84de883aeede44
zxlhhyccc <zxlhhy@gmail.com>
2020-03-22 10:45:41 +0800

    update luci-app-flowoffload-master

--- lean/luci-app-flowoffload-master/root/usr/share/dnscache/dnscache-while.sh
+++ lean/luci-app-flowoffload-master/root/usr/share/dnscache/dnscache-while.sh
@@ -2,7 +2,7 @@
 
 sleeptime=60
 logfile="/var/log/dnscache.file"
-adglogfile="/etc/AdGuardHome/data/querylog.json"
+adg_logfile="/etc/AdGuardHome/data/querylog.json"
 dns_enable=$(uci get flowoffload.@flow[0].dns 2>/dev/null)
 dnscache_enable=$(uci get flowoffload.@flow[0].dnscache_enable 2>/dev/null)
 
@@ -12,38 +12,39 @@ if [ $logrow -ge 500 ];then
     cat /dev/null > ${logfile}
     echo "$curtime Log条数超限，清空处理！" >> ${logfile}
 fi
-
-adglogrow=$(grep -c "" ${adglogfile})
-if [ $adglogrow -ge 5000 ];then
-    cat /dev/null > ${adglogfile}
-    echo "$curtime adgLog条数超限，清空处理！" >> ${logfile}
+if [ $dnscache_enable = "3" ];then
+	adg_logrow=$(grep -c "" ${adg_logfile})
+	if [ $adg_logrow -ge 500 ];then
+		cat /dev/null > ${adg_logfile}
+		echo "$curtime Log条数超限，清空处理！" >> ${adg_logfile}
+	fi
 fi
-
 }
 
 while [ $dns_enable -eq 1 ];
 do
 curtime=`date "+%H:%M:%S"`
 echo "$curtime online! "
-clean_log
 if [ $dns_enable -eq 1 ]; then
-	if [ $dnscache_enable = "3" ];  then
-		if ! pidof AdGuardHome>/dev/null; then
-			AdGuardHome -c /var/etc/AdGuardHome/AdGuardHome.yaml -w /var/etc/AdGuardHome -h 0.0.0.0 -p 3333 >/dev/null 2>&1 &
+	if [ $dnscache_enable = "3" ];then
+		if ! pidof AdGuardHome>/dev/null;then
+			AdGuardHome -c /etc/AdGuardHome/AdGuardHome.yaml -w /etc/AdGuardHome &
 			echo "$curtime 重启服务！" >> ${logfile}
 		fi
 	else
 		if ! pidof dnscache>/dev/null; then
-			if [ $dnscache_enable = "1" ];  then
-			/usr/sbin/dnscache -c /var/etc/dnscache.conf -d
-			elif [ $dnscache_enable = "2" ];  then
-			dnscache -f /var/run/dnscache/dnscache.conf -d
+			if [ $dnscache_enable = "1" ];then
+				/usr/sbin/dnscache -c /var/etc/dnscache.conf &
+			elif [ $dnscache_enable = "2" ];then
+				dnscache -f /var/run/dnscache/dnscache.conf &
 			fi
 			echo "$curtime 重启服务！" >> ${logfile}
 		fi
 	fi
 fi
 
+clean_log
 sleep ${sleeptime}
 continue
 done
+
