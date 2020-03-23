e3a8846c5ed93f91b3c733dd833cde7d62c0ec38
zxlhhyccc <zxlhhy@gmail.com>
2020-03-24 01:42:15 +0800

    update luci-app-flowoffload-master

--- lean/luci-app-flowoffload-master/luasrc/model/cbi/flowoffload.lua
+++ lean/luci-app-flowoffload-master/luasrc/model/cbi/flowoffload.lua
@@ -1,7 +1,9 @@
 local m,s,o
 local SYS  = require "luci.sys"
-local trport = 3001
+local trport = 3333
 local button = ""
+local version = "4.14.131"
+
 if luci.sys.call("pidof AdGuardHome >/dev/null") == 0 then
 	button = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type=\"button\" value=\" " .. translate("Open Web Interface") .. " \" onclick=\"window.open('http://'+window.location.hostname+':" .. trport .. "')\"/>"
 end
@@ -26,22 +28,73 @@ hw.rmempty = true
 hw.description = translate("Enable Hardware NAT (depends on hw capability like MTK 762x)")
 hw:depends("flow_offloading", 1)
 
-fc = s:option(Flag, "fullcone_nat", translate("Fullcone NAT"))
-fc.default = 1
-fc.rmempty = true
-fc.description = translate("Enable Fullcone NAT")
-
-bbr = s:option(Flag, "bbr", translate("Enable BBR"))
-bbr.default = 0
+bbr = s:option(ListValue, "bbr", translate("TCP Congestion Control Algorithm"))
+bbr:value("default", translate("default"))
+bbr:value("bbr", translate("BBR"))
+if nixio.fs.access("/lib/modules/" .. version .. "/tcp_bbr_mod.ko") then
+bbr:value("tcp_bbr_mod", translate("BBR_mod"))
+end
+if nixio.fs.access("/lib/modules/" .. version .. "/tcp_bbr_bbrplus.ko") then
+bbr:value("tcp_bbr_bbrplus", translate("bbr_bbrplus"))
+end
+if nixio.fs.access("/lib/modules/" .. version .. "/tcp_bbr_tsunami.ko") then
+bbr:value("tcp_bbr_tsunami", translate("bbr_tsunami"))
+end
+if nixio.fs.access("/lib/modules/" .. version .. "/tcp_bbr_nanqinlang.ko") then
+bbr:value("nanqinlang", translate("bbr_nanqinlang"))
+end
+if nixio.fs.access("/lib/modules/" .. version .. "/tcp_bic.ko") then
+bbr:value("bic", translate("bic"))
+end
+if nixio.fs.access("/lib/modules/" .. version .. "/tcp_highspeed.ko") then
+bbr:value("highspeed", translate("hstcp"))
+end
+if nixio.fs.access("/lib/modules/" .. version .. "/tcp_htcp.ko") then
+bbr:value("htcp", translate("htcp"))
+end
+if nixio.fs.access("/lib/modules/" .. version .. "/tcp_hybla.ko") then
+bbr:value("hybla", translate("hybla"))
+end
+if nixio.fs.access("/lib/modules/" .. version .. "/tcp_illinois.ko") then
+bbr:value("illinois", translate("illinois"))
+end
+if nixio.fs.access("/lib/modules/" .. version .. "/tcp_lp.ko") then
+bbr:value("lp", translate("lp"))
+end
+if nixio.fs.access("/lib/modules/" .. version .. "/tcp_scalable.ko") then
+bbr:value("scalable", translate("scalable"))
+end
+if nixio.fs.access("/lib/modules/" .. version .. "/tcp_veno.ko") then
+bbr:value("vegas", translate("veno"))
+end
+if nixio.fs.access("/lib/modules/" .. version .. "/tcp_vegas.ko") then
+bbr:value("vegas", translate("vegas"))
+end
+if nixio.fs.access("/lib/modules/" .. version .. "/tcp_westwood.ko") then
+bbr:value("westwood", translate("westwood"))
+end
+if nixio.fs.access("/lib/modules/" .. version .. "/tcp_yeah.ko") then
+bbr:value("yeah", translate("yeah"))
+end
+bbr.default = "cubic"
 bbr.rmempty = false
 bbr.description = translate("Bottleneck Bandwidth and Round-trip propagation time (BBR)")
+
+o = s:option(Flag, "free_memory", translate("Automatic Free Memory"))
+o.default = 0
+o.rmempty = false
+
+aaaa = s:option(Flag, "filter_aaaa", translate("Filter AAAA"))
+aaaa.default = 0
+aaaa.rmempty = false
+aaaa.description = translate("Dnsmasq rejects IPv6 parsing and optimizes domestic complex dual-stack network")
 
 dns = s:option(Flag, "dns", translate("DNS Acceleration"))
 dns.default = 0
 dns.rmempty = false
 dns.description = translate("Enable DNS Cache Acceleration and anti ISP DNS pollution")
 
-o = s:option(ListValue, "dnscache_enable", translate("Resolve Dns Mode"), translate("AdGuardHome's login username/passwd: AdGuardHome, web console addr: IP:3001") .. button)
+o = s:option(ListValue, "dnscache_enable", translate("Resolve Dns Mode"), translate("AdGuardHome After setting up, shut down DNS acceleration normally and save configuration file") .. button)
 o:value("1", translate("Use Pdnsd query and cache"))
 if nixio.fs.access("/usr/bin/dnsforwarder") then
 o:value("2", translate("Use dnsforwarder query and cache"))
@@ -51,6 +104,11 @@ o:value("3", translate("Use AdGuardHome query and cache"))
 end
 o.default = 1
 o:depends("dns", 1)
+
+o = s:option(Flag, "mem_mode", translate("Memory mode operation"))
+o.default = 0
+o.rmempty = false
+o:depends("dnscache_enable", 3)
 
 o = s:option(Value, "dns_server", translate("Upsteam DNS Server"))
 o.default = "114.114.114.114,114.114.115.115,223.5.5.5,223.6.6.6,180.76.76.76,119.29.29.29,119.28.28.28,1.2.4.8,210.2.4.8"
@@ -62,5 +120,6 @@ o = s:option(Value, "ipv6dns_server", translate("Upsteam IPV6 DNS Server"))
 o.default = "2001:4860:4860::8888,2001:4860:4860::8844,2001:2001::1111,2001:2001::1001,2400:da00::6666,240C::6666,240C::6644"
 o.description = translate("Muitiple IPV6 DNS server can saperate with ','")
 o:depends("dnscache_enable", 2)
+
 
 return m
--- lean/luci-app-flowoffload-master/root/usr/share/dnscache/dnscache-while.sh
+++ lean/luci-app-flowoffload-master/root/usr/share/dnscache/dnscache-while.sh
@@ -2,7 +2,7 @@
 
 sleeptime=60
 logfile="/var/log/dnscache.file"
-adg_logfile="/etc/AdGuardHome/data/querylog.json"
+adglogfile="/etc/AdGuardHome/data/querylog.json"
 dns_enable=$(uci get flowoffload.@flow[0].dns 2>/dev/null)
 dnscache_enable=$(uci get flowoffload.@flow[0].dnscache_enable 2>/dev/null)
 
@@ -12,39 +12,38 @@ if [ $logrow -ge 500 ];then
     cat /dev/null > ${logfile}
     echo "$curtime Log条数超限，清空处理！" >> ${logfile}
 fi
-if [ $dnscache_enable = "3" ];then
-	adg_logrow=$(grep -c "" ${adg_logfile})
-	if [ $adg_logrow -ge 500 ];then
-		cat /dev/null > ${adg_logfile}
-		echo "$curtime Log条数超限，清空处理！" >> ${adg_logfile}
-	fi
+
+adglogrow=$(grep -c "" ${adglogfile})
+if [ $adglogrow -ge 5000 ];then
+    cat /dev/null > ${adglogfile}
+    echo "$curtime adgLog条数超限，清空处理！" >> ${logfile}
 fi
+
 }
 
 while [ $dns_enable -eq 1 ];
 do
 curtime=`date "+%H:%M:%S"`
 echo "$curtime online! "
+clean_log
 if [ $dns_enable -eq 1 ]; then
-	if [ $dnscache_enable = "3" ];then
-		if ! pidof AdGuardHome>/dev/null;then
-			AdGuardHome -c /etc/AdGuardHome/AdGuardHome.yaml -w /etc/AdGuardHome &
+	if [ $dnscache_enable = "3" ];  then
+		if ! pidof AdGuardHome>/dev/null; then
+			AdGuardHome -c /var/etc/AdGuardHome/AdGuardHome.yaml -w /var/etc/AdGuardHome -h 0.0.0.0 -p 3333 >/dev/null 2>&1 &
 			echo "$curtime 重启服务！" >> ${logfile}
 		fi
 	else
 		if ! pidof dnscache>/dev/null; then
-			if [ $dnscache_enable = "1" ];then
-				/usr/sbin/dnscache -c /var/etc/dnscache.conf &
-			elif [ $dnscache_enable = "2" ];then
-				dnscache -f /var/run/dnscache/dnscache.conf &
+			if [ $dnscache_enable = "1" ];  then
+			/usr/sbin/dnscache -c /var/etc/dnscache.conf -d
+			elif [ $dnscache_enable = "2" ];  then
+			dnscache -f /var/run/dnscache/dnscache.conf -d
 			fi
 			echo "$curtime 重启服务！" >> ${logfile}
 		fi
 	fi
 fi
 
-clean_log
 sleep ${sleeptime}
 continue
 done
-
