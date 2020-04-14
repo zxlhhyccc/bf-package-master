cb1271b7e033020ba174e7a0f7adcb4143b03e30
zxlhhyccc <zxlhhy@gmail.com>
2020-04-14 13:55:21 +0800

    revert luci-app-qosv4

--- ntlf9t/luci-app-qosv4/files/usr/lib/lua/luci/controller/qosv4.lua
+++ ntlf9t/luci-app-qosv4/files/usr/lib/lua/luci/controller/qosv4.lua
@@ -1,6 +1,8 @@
 module("luci.controller.qosv4", package.seeall)
 
 function index()
+     require("luci.i18n")
+     luci.i18n.loadc("qosv4")
     local fs = luci.fs or nixio.fs
     if not fs.access("/etc/config/qosv4") then
 		return
