edcb59c4b720bb54caa022702ac67318e0703070
zxlhhyccc <zxlhhy@gmail.com>
2020-12-16 02:20:43 +0800

    luci-app-control-openvpn-server:update@20201216

--- ntlf9t/luci-app-control-openvpn-server/luasrc/controller/openvpn-server.lua
+++ ntlf9t/luci-app-control-openvpn-server/luasrc/controller/openvpn-server.lua
@@ -4,7 +4,9 @@ function index()
 		return
 	end
 	entry({"admin","vpn"},firstchild(),"VPN",45).dependent=false
-	entry({"admin","vpn","openvpn-server"},firstchild(),_("OpenVPN Server")).dependent=false
+	local e=entry({"admin","vpn","openvpn-server"},firstchild(),_("OpenVPN Server"),1)
+	e.dependent=false
+	e.acl_depends={ "luci-app-control-openvpn-server" }
 	entry({"admin","vpn","openvpn-server","base"},cbi("openvpn-server/base"),_("Base Setting"),1)
 	entry({"admin","vpn","openvpn-server","client"},form("openvpn-server/client"),_("Client configuration"),2)
 	entry({"admin","vpn","openvpn-server","passwd"},form("openvpn-server/passwd"),_("Username and Password"),3)
