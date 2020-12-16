4196a7dc58dee6f7a7dcf1e4b273ecc32e73b371
zxlhhyccc <zxlhhy@gmail.com>
2020-12-16 13:51:58 +0800

    Fix unable to start SFE with IPv4 only

--- lean/luci-app-turboacc/luasrc/model/cbi/turboacc.lua
+++ lean/luci-app-turboacc/luasrc/model/cbi/turboacc.lua
@@ -13,6 +13,7 @@ s.anonymous = true
 if nixio.fs.access("/lib/modules/" .. kernel_version .. "/xt_FLOWOFFLOAD.ko") then
 sw_flow = s:option(Flag, "sw_flow", translate("Software flow offloading"))
 sw_flow.default = 0
+sw_flow.rmempty = false
 sw_flow.description = translate("Software based offloading for routing/NAT")
 sw_flow:depends("sfe_flow", 0)
 end
@@ -20,6 +21,7 @@ end
 if luci.sys.call("cat /proc/cpuinfo | grep -q MT76") == 0 then
 hw_flow = s:option(Flag, "hw_flow", translate("Hardware flow offloading"))
 hw_flow.default = 0
+hw_flow.rmempty = false
 hw_flow.description = translate("Requires hardware NAT support. Implemented at least for mt76xx")
 hw_flow:depends("sw_flow", 1)
 end
@@ -27,18 +29,21 @@ end
 if nixio.fs.access("/lib/modules/" .. kernel_version .. "/fast-classifier.ko") then
 sfe_flow = s:option(Flag, "sfe_flow", translate("Shortcut-FE flow offloading"))
 sfe_flow.default = 0
+sfe_flow.rmempty = false
 sfe_flow.description = translate("Shortcut-FE based offloading for routing/NAT")
 sfe_flow:depends("sw_flow", 0)
 end
 
 sfe_bridge = s:option(Flag, "sfe_bridge", translate("Bridge Acceleration"))
 sfe_bridge.default = 0
+sfe_bridge.rmempty = false
 sfe_bridge.description = translate("Enable Bridge Acceleration (may be functional conflict with bridge-mode VPN server)")
 sfe_bridge:depends("sfe_flow", 1)
 
 if nixio.fs.access("/proc/sys/net/ipv6") then
 sfe_ipv6 = s:option(Flag, "sfe_ipv6", translate("IPv6 Acceleration"))
 sfe_ipv6.default = 0
+sfe_ipv6.rmempty = false
 sfe_ipv6.description = translate("Enable IPv6 Acceleration")
 sfe_ipv6:depends("sfe_flow", 1)
 end
@@ -46,12 +51,14 @@ end
 if nixio.fs.access("/lib/modules/" .. kernel_version .. "/tcp_bbr.ko") then
 bbr_cca = s:option(Flag, "bbr_cca", translate("BBR CCA"))
 bbr_cca.default = 0
+bbr_cca.rmempty = false
 bbr_cca.description = translate("Using BBR CCA can improve TCP network performance effectively")
 end 
 
 if nixio.fs.access("/lib/modules/" .. kernel_version .. "/xt_FULLCONENAT.ko") then
 fullcone_nat = s:option(Flag, "fullcone_nat", translate("FullCone NAT"))
 fullcone_nat.default = 0
+fullcone_nat.rmempty = false
 fullcone_nat.description = translate("Using FullCone NAT can improve gaming performance effectively")
 end 
 
