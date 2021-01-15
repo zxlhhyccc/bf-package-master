581612ad4a7bb2f39406dd19412e41684adec6e6
zxlhhyccc <zxlhhy@gmail.com>
2021-01-15 19:21:18 +0800

    luci-app-bypass:update@20210115

--- zxlhhyccc/luci-app-bypass/luasrc/model/cbi/bypass/base.lua
+++ zxlhhyccc/luci-app-bypass/luasrc/model/cbi/bypass/base.lua
@@ -72,63 +72,67 @@ o=s:option(ListValue,"dports",translate("Proxy Ports"))
 o:value("1",translate("All Ports"))
 o:value("2",translate("Only Common Ports"))
 
-o=s:option(ListValue,"dns_mode",translate("Foreign Resolve Dns Mode"))
-o:value("0",translate("Use SmartDNS DoH query"))
-o:value("1",translate("Use SmartDNS TCP query"))
+o=s:option(ListValue,"dns_mode_o",translate("Foreign Resolve Dns Mode"))
+o:value("doh",translate("Use SmartDNS DoH query"))
+o:value("tcp",translate("Use SmartDNS TCP query"))
+o.default="tcp"
 
-o=s:option(Value,"dns",translate("Foreign DoH"),
+o=s:option(Value,"doh_dns_o",translate("Foreign DoH"),
 translate("Custom DNS format is https://cloudflare-dns.com/dns-query or https://104.16.249.249/dns-query -http-host cloudflare-dns.com"))
-o:value("","Cloudflare DoH")
+o:value("cloudflare","Cloudflare DoH")
 o:value("google",""..translate("Google").." DoH")
 o:value("quad9","Quad9 DoH")
 o:value("opendns","OpenDNS DoH")
-o:depends("dns_mode",0)
+o.default="cloudflare"
+o:depends("dns_mode_o",doh)
 
 o=s:option(Value,"dns_nf",translate("Netflix DoH"),
 translate("Do not use the same DNS server as the global server"))
-o:value("",""..translate("Google").." DoH")
+o:value("google",""..translate("Google").." DoH")
 o:value("cloudflare","Cloudflare DoH")
 o:value("quad9","Quad9 DoH")
 o:value("opendns","OpenDNS DoH")
+o.default="google"
 for _,key in pairs(key_table) do o:depends({nf_server=key,dns_mode=0}) end
 
-o=s:option(Value,"tcp_dns",translate("Foreign DNS"),
+o=s:option(Value,"tcp_dns_o",translate("Foreign DNS"),
 translate("Custom DNS format is 1.1.1.1:53,1.0.0.1 ,Port optional"))
-o:value("","1.1.1.1,1.0.0.1 (Cloudflare DNS)")
+o:value("1.1.1.1,1.0.0.1","1.1.1.1,1.0.0.1 (Cloudflare DNS)")
 o:value("8.8.8.8,8.8.4.4","8.8.8.8,8.8.4.4 (Google DNS)")
 o:value("9.9.9.9,149.112.112.112","9.9.9.9,149.112.112.112 (Quad9 DNS)")
 o:value("208.67.222.222,208.67.220.220","208.67.222.222,208.67.220.220 (OpenDNS)")
-o:depends("dns_mode",1)
+o.default="1.1.1.1,1.0.0.1"
+o:depends("dns_mode_o",tcp)
 
 o=s:option(Value,"tcp_dns_nf",translate("Netflix DNS"),
 translate("Do not use the same DNS server as the global server"))
-o:value("","8.8.8.8,8.8.4.4 (Google DNS)")
+o:value("8.8.8.8,8.8.4.4","8.8.8.8,8.8.4.4 (Google DNS)")
 o:value("1.1.1.1,1.0.0.1","1.1.1.1,1.0.0.1 (Cloudflare DNS)")
 o:value("9.9.9.9,149.112.112.112","9.9.9.9,149.112.112.112 (Quad9 DNS)")
 o:value("208.67.222.222,208.67.220.220","208.67.222.222,208.67.220.220 (OpenDNS)")
-for _,key in pairs(key_table) do o:depends({nf_server=key,dns_mode=1}) end
+o.default="8.8.8.8,8.8.4.4"
+for _,key in pairs(key_table) do o:depends({nf_server=key,dns_mode_o=tcp}) end
 
-o=s:option(ListValue,"dns_mode_l",translate("Domestic Resolve Dns Mode"),
+o=s:option(ListValue,"dns_mode_d",translate("Domestic Resolve Dns Mode"),
 translate("If DoH resolution is not normal,use UDP mode and select ISP DNS"))
-o:value("0",translate("Use SmartDNS DoH query"))
-o:value("1",translate("Use SmartDNS UDP query"))
+o:value("doh",translate("Use SmartDNS DoH query"))
+o:value("udp",translate("Use SmartDNS UDP query"))
+o.default="udp"
 
-o=s:option(Value,"dns_l",translate("Domestic DoH"),
+o=s:option(Value,"doh_dns_d",translate("Domestic DoH"),
 translate("Custom DNS format is https://dns.alidns.com/dns-query or https://223.5.5.5/dns-query"))
-o:value("",""..translate("Ali").." DoH")
-o:value("1","Dnspod DoH")
-o:depends("dns_mode_l",0)
+o:value("alidns",""..translate("Ali").." DoH")
+o:value("dnspod","Dnspod DoH")
+o.default="alidns"
+o:depends("dns_mode_d",doh)
 
-o=s:option(Flag,"isp_dns",translate("ISP DNS"),
-translate("Use ISP DNS to resolve DoH domain"))
-o:depends("dns_mode_l",0)
-
-o=s:option(Value,"udp_dns",translate("Domestic DNS"),
+o=s:option(Value,"udp_dns_d",translate("Domestic DNS"),
 translate("Custom DNS format is 223.5.5.5:53,223.6.6.6 ,Port optional"))
 o:value("",translate("ISP DNS"))
 o:value("223.5.5.5,223.6.6.6","223.5.5.5,223.6.6.6 ("..translate("Ali").." DNS)")
 o:value("119.29.29.29,119.28.28.28","119.29.29.29,119.28.28.28 (Dnspod DNS)")
 o:value("114.114.114.114,114.114.115.115","114.114.114.114,114.114.115.115 (114 DNS)")
-o:depends("dns_mode_l",1)
+o.default="223.5.5.5,223.6.6.6"
+o:depends("dns_mode_d",udp)
 m:section(SimpleSection).template = 'bypass/status_bottom'
 return m
