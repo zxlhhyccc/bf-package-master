e2a331a68ad98c5fc6cc92c638baafb766ea902d
zxlhhyccc <zxlhhy@gmail.com>
2021-10-16 00:15:39 +0800

    luci-app-syncthing-master:Fixed the inability to stop and log in to the admin page

--- zxlhhyccc/luci-app-syncthing-master/luasrc/controller/syncthing.lua
+++ zxlhhyccc/luci-app-syncthing-master/luasrc/controller/syncthing.lua
@@ -5,13 +5,13 @@ function index()
 		return
 	end
 
-	entry({"admin", "services", "syncthing"}, cbi("syncthing"), _("Syncthing"), 10).dependent = true
-	entry({"admin","services","syncthing","status"},call("act_status")).leaf=true
+	entry({"admin", "nas", "syncthing"}, cbi("syncthing"), _("Syncthing"), 10).dependent = true
+	entry({"admin","nas","syncthing","status"},call("act_status")).leaf = true
 end
 
 function act_status()
   local e={}
-  e.running=luci.sys.call("pgrep syncthing >/dev/null")==0
+  e.running=luci.sys.call("pgrep -f syncthing >/dev/null")==0
   luci.http.prepare_content("application/json")
   luci.http.write_json(e)
 end
--- zxlhhyccc/luci-app-syncthing-master/luasrc/model/cbi/syncthing.lua
+++ zxlhhyccc/luci-app-syncthing-master/luasrc/model/cbi/syncthing.lua
@@ -5,15 +5,17 @@ m = Map("syncthing", translate("Syncthing Synchronization Tool"))
 m:section(SimpleSection).template  = "syncthing/syncthing_status"
 
 s = m:section(TypedSection, "syncthing")
-
+s.addremove = false
 s.anonymous = true
 
-s:option(Flag, "enabled", translate("Enable"))
+o = s:option(Flag, "enabled", translate("Enabled"))
+o.default = 0
+o.rmempty = false
 
 gui_address = s:option(Value, "gui_address", translate("GUI access address"))
-gui_address.description = translate("Use 0.0.0.0 to monitor all access.")
-gui_address.default = "http://127.0.0.1:8384"
-gui_address.placeholder = "http://127.0.0.1:8384"
+gui_address.description = translate("Use 0.0.0.0:8384 to monitor all access.")
+gui_address.default = "http://0.0.0.0:8384"
+gui_address.placeholder = "http://0.0.0.0:8384"
 gui_address.rmempty = false
 
 home = s:option(Value, "home", translate("Configuration file directory"))
@@ -22,11 +24,12 @@ home.default = "/etc/syncthing"
 home.placeholder = "/etc/syncthing"
 home.rmempty = false
 
-user = s:option(Value, "user", translate("User"))
+user = s:option(ListValue, "user", translate("User"))
 user.description = translate("The default is syncthing, but it may cause permission denied. Syncthing officially does not recommend running as root.")
-user.default = "syncthing"
-user.placeholder = "syncthing"
-user.rmempty = false
+user:value("", translate("syncthing"))
+for u in luci.util.execi("cat /etc/passwd | cut -d ':' -f1") do
+	user:value(u)
+end
 
 macprocs = s:option(Value, "macprocs", translate("Thread limit"))
 macprocs.description = translate("0 to match the number of CPUs (default), >0 to explicitly specify concurrency.")
@@ -42,6 +45,5 @@ nice.placeholder = "19"
 nice.datatype="range(0,19)"
 nice.rmempty = false
 
-s.rmempty = false
 
 return m
