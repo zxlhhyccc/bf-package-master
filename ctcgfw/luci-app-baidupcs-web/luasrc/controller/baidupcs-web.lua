63e43781770e47a58fc0a39d8b5d768791958c0b
zxlhhyccc <zxlhhy@gmail.com>
2021-11-28 15:11:38 +0800

    luci-app-baidupcs-web:add Display version number

--- ctcgfw/luci-app-baidupcs-web/luasrc/controller/baidupcs-web.lua
+++ ctcgfw/luci-app-baidupcs-web/luasrc/controller/baidupcs-web.lua
@@ -12,6 +12,7 @@ function act_status()
 	local e={}
 	e.running=luci.sys.call("pgrep baidupcs-web >/dev/null")==0
 	-- e.port=luci.sys.exec("uci get baidupcs-web.config.port")
+	e.bin_version = luci.sys.exec("baidupcs-web --version  2>/dev/null | grep -m 1 -E '[0-9]+[.][0-9.]+' -o")
 	luci.http.prepare_content("application/json")
 	luci.http.write_json(e)
 end
