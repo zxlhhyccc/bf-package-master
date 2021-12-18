d227841552fc70f5a5c8e99c881db7c0f96625fb
zxlhhyccc <zxlhhy@gmail.com>
2021-12-18 22:32:43 +0800

    luci-app-npc-client:fix typo erro

--- zxlhhyccc/luci-app-npc-client/luasrc/controller/npc.lua
+++ zxlhhyccc/luci-app-npc-client/luasrc/controller/npc.lua
@@ -9,12 +9,12 @@ function index()
 	e=entry({"admin","services","npc"},cbi("npc"),_("Nps Client"))
 	e.i18n="npc"
 	e.dependent=true
-	entry({"admin","services","npc","status"},call("status")).leaf=true
+	entry({"admin","services","npc","status"},call("act_status")).leaf=true
 end
 
-function status()
-	local e={}
-	e.running=luci.sys.call("pgrep npc > /dev/null")==0
+function act_status()
+	local e = {}
+	e.running=luci.sys.call("pgrep npc > /dev/null") == 0
 	e.bin_version = luci.sys.exec("npc --version  2>/dev/null | grep -m 1 -E '[0-9]+[.][0-9.]+' -o")
 	luci.http.prepare_content("application/json")
 	luci.http.write_json(e)
