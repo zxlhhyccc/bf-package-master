42c6e128efc4e3f76f13941d936907ad76486005
zxlhhyccc <zxlhhy@gmail.com>
2022-01-10 20:33:45 +0800

    luci-app-kodexplorer: sync to upstream

--- lean/luci-app-kodexplorer/luasrc/controller/kodexplorer.lua
+++ lean/luci-app-kodexplorer/luasrc/controller/kodexplorer.lua
@@ -1,4 +1,4 @@
--- Copyright 2018-2020 Lienol <lawlienol@gmail.com>
+-- Copyright 2018-2022 Lienol <lawlienol@gmail.com>
 module("luci.controller.kodexplorer", package.seeall)
 
 local http = require "luci.http"
@@ -8,7 +8,10 @@ function index()
     if not nixio.fs.access("/etc/config/kodexplorer") then return end
 
     entry({"admin", "nas"}, firstchild(), "NAS", 44).dependent = false
-    entry({"admin", "nas", "kodexplorer"}, cbi("kodexplorer/settings"), _("KodExplorer"), 3).dependent = true
+
+    e = entry({"admin", "nas", "kodexplorer"}, cbi("kodexplorer/settings"), _("KodExplorer"), 3)
+    e.dependent = true
+    e.acl_depends = { "luci-app-kodexplorer" }
 
     entry({"admin", "nas", "kodexplorer", "check"}, call("action_check")).leaf = true
     entry({"admin", "nas", "kodexplorer", "download"}, call("action_download")).leaf = true
@@ -23,7 +26,7 @@ end
 function act_status()
     local e = {}
     e.nginx_status = luci.sys.call("ps -w | grep nginx | grep kodexplorer | grep -v grep > /dev/null") == 0
-    e.php_status = luci.sys.call("ps -w | grep php | grep kodexplorer | grep -v grep > /dev/null") ==  0
+    e.php_status = luci.sys.call("ps -w | grep php | grep kodexplorer | grep -v grep > /dev/null") == 0
     http_write_json(e)
 end
 
