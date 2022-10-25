a3db99866293bc64278aa0df8094d14ce6d765f2
zxlhhyccc <zxlhhyccc@gmail.com>
2022-10-25 21:19:04 +0800

    autocore: implement tempinfo display

--- lean/autocore/files/generic/10_system.js
+++ lean/autocore/files/generic/10_system.js
@@ -78,6 +78,22 @@ return baseclass.extend({
 			);
 		}
 
+		// TODO: This is ugly
+		var projectlink = document.createElement('a');
+		projectlink.append('OpenWrt');
+		projectlink.href = 'https://github.com/openwrt';
+		projectlink.target = '_blank';
+
+		var corelink = document.createElement('a');
+		corelink.append('Core');
+		corelink.href = 'https://github.com/openwrt/openwrt';
+		corelink.target = '_blank';
+
+		var sourcelink = document.createElement('placeholder');
+		sourcelink.append(projectlink);
+		sourcelink.append(' / ');
+		sourcelink.append(corelink);
+
 		var fields = [
 			_('Hostname'),         boardinfo.hostname,
 			_('Model'),            boardinfo.model + cpubench.cpubench,
@@ -93,6 +109,7 @@ return baseclass.extend({
 				systeminfo.load[2] / 65535.0
 			) : null,
 			_('CPU usage (%)'),    cpuusage.cpuusage
+			_('Source Code'),      sourcelink
 		];
 
 		if (tempinfo.tempinfo) {
