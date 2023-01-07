0aba6aae36138f8ad638273502ed2dba55337b07
zxlhhyccc <zxlhhyccc@gmail.com>
2023-01-07 13:45:05 +0800

    luci-app-ssr-plus-mosdns: Add DNS-CDN list workround for Bilibili and Apple domain

--- zxlhhyccc/helloworld-klever1988/luci-app-ssr-plus-mosdns/root/etc/ssrplus/blockipv6.sh
+++ zxlhhyccc/helloworld-klever1988/luci-app-ssr-plus-mosdns/root/etc/ssrplus/blockipv6.sh
@@ -90,6 +90,7 @@ ipset -! add blockipv6 2a05:d050:a000::/40
 ipset -! add blockipv6 2a05:d078:9000::/40
 ipset -! add blockipv6 2406:da68:7000::/40
 ipset -! add blockipv6 2406:daf9:e000::/40
+ipset -! add blockipv6 2600:1f68:a400::/40
 ipset -! add blockipv6 2a05:d079:4000::/40
 ipset -! add blockipv6 2406:daf0:6000::/40
 ipset -! add blockipv6 2600:1fa0:6000::/40
@@ -148,6 +149,7 @@ ipset -! add blockipv6 2600:1ff0:1000::/40
 ipset -! add blockipv6 2a05:d078:a000::/40
 ipset -! add blockipv6 2a05:d050:4000::/40
 ipset -! add blockipv6 2406:daf0:4000::/40
+ipset -! add blockipv6 2600:1ff0:a400::/40
 ipset -! add blockipv6 2600:1fa0:5000::/40
 ipset -! add blockipv6 2406:daa0:e000::/40
 ipset -! add blockipv6 2600:1f68:2000::/40
@@ -167,6 +169,7 @@ ipset -! add blockipv6 2404:c2c0:2400::/40
 ipset -! add blockipv6 2600:1ffa:e000::/40
 ipset -! add blockipv6 2406:daf0:e000::/40
 ipset -! add blockipv6 2600:1ffa:8000::/40
+ipset -! add blockipv6 2600:1fa0:a400::/40
 ipset -! add blockipv6 2406:daf9:1000::/40
 ipset -! add blockipv6 2a05:d034:6000::/40
 ipset -! add blockipv6 2a05:d070:9000::/40
