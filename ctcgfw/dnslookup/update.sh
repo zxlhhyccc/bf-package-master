6c10602d17c2f438ade75d3e9dfd4b068c5255f8
zxlhhyccc <zxlhhyccc@gmail.com>
2024-03-17 01:46:18 +0800

    add update script

---
+++ ctcgfw/dnslookup/update.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -x
+
+export CURDIR="$(cd "$(dirname $0)"; pwd)"
+
+function update() {
+	local type="$1"
+	local repo="$2"
+	local res="$3"
+	local tag ver sha old_hash line commit
+
+	tag="$(curl -H "Authorization: $GITHUB_TOKEN" -sL "https://api.github.com/repos/$repo/releases/latest" | jq -r ".tag_name" | sed 's/v//')"
+	[ -n "$tag" ] || return 1
+
+        ver="$(awk -F 'PKG_VERSION:=' '/PKG_VERSION:/{gsub("\"","",$2);print $2}' "$CURDIR/Makefile")"
+
+	[ "$tag" != "$ver" ] || return 2
+	
+	line="$(awk "/PKG_VERSION:=/ {print NR}" "$CURDIR/Makefile")"
+	sed -i -e "$((line))s/PKG_VERSION:=.*/PKG_VERSION:=$tag/" "$CURDIR/Makefile"
+
+	sha="$(curl -sL https://codeload.github.com/ameshkov/dnslookup/tar.gz/v$tag | shasum -a 256 | awk '{print $1}')"
+	[ -n "$sha" ] || return 1
+	
+	old_hash="$(awk -F 'PKG_MIRROR_HASH:=' '/PKG_MIRROR_HASH:/{gsub("\"","",$2);print $2}' "$CURDIR/Makefile")"
+	line="$(awk "/PKG_MIRROR_HASH:=/ {print NR}" "$CURDIR/Makefile")"	
+	[ "$sha" != "$old_sha" ] || return 2
+	
+	   sed -i -e "$((line))s/PKG_MIRROR_HASH:=.*/PKG_MIRROR_HASH:=$sha/" "$CURDIR/Makefile"
+
+	  commit="$(git ls-remote https://github.com/txthinking/brook.git HEAD | cut -f1)"
+}
+
+update "dnslookup" "ameshkov/dnslookup" "dnslookup-"$(awk -F 'PKG_VERSION:=' '/PKG_VERSION:/{gsub("\"","",$2);print $2}' "$CURDIR/Makefile")"" 
