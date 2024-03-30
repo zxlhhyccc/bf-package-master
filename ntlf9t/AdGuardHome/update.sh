e42da83006fdcd06db1630ba3db783a746aa3b41
zxlhhyccc <zxlhhyccc@gmail.com>
2024-03-30 16:41:38 +0800

    AdGuardHome: update script

--- ntlf9t/AdGuardHome/update.sh
+++ ntlf9t/AdGuardHome/update.sh
@@ -14,7 +14,7 @@ function update() {
 	tag="$(curl -H "Authorization: $GITHUB_TOKEN" -sL "https://api.github.com/repos/$repo/releases/latest" | jq -r ".tag_name" | sed 's/v//')"
 	[ -n "$tag" ] || return 1
 
-        ver="$(awk -F 'PKG_VERSION:=' '/PKG_VERSION:/{gsub("\"","",$2);print $2}' "$CURDIR/Makefile")"
+        ver="$(awk -F "PKG_VERSION:=" '{print $2}' "$CURDIR/Makefile" | xargs)"
 
 	[ "$tag" != "$ver" ] || return 2
 	
@@ -24,8 +24,9 @@ function update() {
 	# 获取哈希值
 	sha="$(curl -sL https://codeload.github.com/$repo/tar.gz/v$tag | shasum -a 256 | awk '{print $1}')"
 	[ -n "$sha" ] || return 1
-	
-	old_sha="$(awk -F 'PKG_HASH:=' '/PKG_HASH:/{gsub("\"","",$2);print $2}' "$CURDIR/Makefile")"
+
+        old_sha=="$(awk -F "PKG_HASH:=" '{print $2}' "$CURDIR/Makefile" | xargs)"
+
 	line="$(awk "/PKG_HASH:=/ {print NR}" "$CURDIR/Makefile")"	
 	[ "$sha" != "$old_sha" ] || return 2
 	
@@ -34,12 +35,13 @@ function update() {
 	# 获取AdGuardHome-frontend值
 	frontend_sha="$(curl -sL https://github.com/$repo/releases/download/v$tag/AdGuardHome_frontend.tar.gz | shasum -a 256 | awk '{print $1}')"
 	[ -n "$frontend_sha" ] || return 1
-	
-        frontend_old_sha="$(awk -F 'HASH:=' '/HASH:/{gsub("\"","",$2);print $2}' "$CURDIR/Makefile")"
+
+	line="$(awk "/FILE:=\\$\(${res}_FILE\)/ {print NR}" "$CURDIR/Makefile")"
+
+        frontend_old_sha="$(awk -F "HASH:=" -v next_line="$((line + 1))" 'NR==next_line {print $2}' "$CURDIR/Makefile" | xargs)"
 
 	[ "$frontend_sha" != "$frontend_old_sha" ] || return 3
-	
-	line="$(awk "/FILE:=\\$\(${res}_FILE\)/ {print NR}" "$CURDIR/Makefile")"
+
 	sed -i -e "$((line + 1))s/HASH:=.*/HASH:=$frontend_sha/" "$CURDIR/Makefile"
 
 	# 获取commit值
