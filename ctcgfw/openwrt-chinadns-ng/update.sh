03df47550de78acf04592af95ae49991b8c85d44
zxlhhyccc <zxlhhyccc@gmail.com>
2024-03-30 16:11:42 +0800

    chinadns-ng: download cache named after $(PKG_VERSION)

--- ctcgfw/openwrt-chinadns-ng/update.sh
+++ ctcgfw/openwrt-chinadns-ng/update.sh
@@ -24,33 +24,6 @@ function update_version() {
 update_version "chinadns-ng"  "zfl9/chinadns-ng"
 
 # 获取哈希值
-if grep -qE "i386|x86_64" "$CURDIR/Makefile"; then
-function update() {
-	local type="$1"
-	local repo="$2"
-	local res="$3"
-	local tag sha old_sha line
-
-	tag="$(curl -H "Authorization: $GITHUB_TOKEN" -sL "https://api.github.com/repos/$repo/releases/latest" | jq -r ".tag_name")"
-	[ -n "$tag" ] && {
-	sha="$(curl -sL https://github.com/$repo/releases/download/$tag/chinadns-ng@${type}@${res}@fast+lto | shasum -a 256 | awk '{print $1}')"
-
-	[ -n "$sha" ] || return 1
-
-	old_sha="$(awk -F 'PKG_HASH:=' '/PKG_HASH:/{gsub("\"","",$2);print $2}' "$CURDIR/Makefile")"
-
-	[ "$sha" != "$old_sha" ] || return 2	
-
-	line="$(awk "/PKG_SOURCE:=chinadns-ng@${type}@${res}@fast\+lto/ {print NR}" "$CURDIR/Makefile")"
-
-	sed -i -e "$((line + 1))s/PKG_HASH:=.*/PKG_HASH:=$sha/" "$CURDIR/Makefile"
-	}
-}
-
-update "i386-linux-musl" "zfl9/chinadns-ng" "i686"
-update "x86_64-linux-musl" "zfl9/chinadns-ng" "x86_64"
-fi
-
 if grep -qE "aarch64|arm|mips|mipsel" "$CURDIR/Makefile"; then
 function _update() {
 	local type="$1"
@@ -61,15 +34,15 @@ function _update() {
 
 	tag="$(curl -H "Authorization: $GITHUB_TOKEN" -sL "https://api.github.com/repos/$repo/releases/latest" | jq -r ".tag_name")"
 	[ -n "$tag" ] && {
-	sha="$(curl -sL https://github.com/$repo/releases/download/$tag/chinadns-ng@${type}@${res}+${reg}@fast+lto | shasum -a 256 | awk '{print $1}')"
+	sha="$(curl -sL https://github.com/$repo/releases/download/$tag/chinadns-ng@${type}@${res}+${reg}@fast+lto | sha256sum | awk '{print $1}')"
 
 	[ -n "$sha" ] || return 1
+
+	line="$(awk "/PKG_ARCH:=chinadns-ng@${type}@${res}\+${reg}@fast\+lto/ {print NR}" "$CURDIR/Makefile")"
 	
-	old_sha="$(awk -F 'PKG_HASH:=' '/PKG_HASH:/{gsub("\"","",$2);print $2}' "$CURDIR/Makefile")"
+	old_sha="$(awk -F "PKG_HASH:=" -v next_line="$((line + 1))" 'NR==next_line {print $2}' "$CURDIR/Makefile" | xargs)"
 
 	[ "$sha" != "$old_sha" ] || return 2
-
-	line="$(awk "/PKG_SOURCE:=chinadns-ng@${type}@${res}\+${reg}@fast\+lto/ {print NR}" "$CURDIR/Makefile")"
 
 	sed -i -e "$((line + 1))s/PKG_HASH:=.*/PKG_HASH:=$sha/" "$CURDIR/Makefile"
 	}
@@ -80,4 +53,31 @@ _update "arm-linux-musleabi" "zfl9/chinadns-ng" "generic" "v7a"
 _update "arm-linux-musleabihf" "zfl9/chinadns-ng" "generic" "v7a"
 _update "mips-linux-musl" "zfl9/chinadns-ng" "mips32" "soft_float"
 _update "mipsel-linux-musl" "zfl9/chinadns-ng" "mips32" "soft_float"
+fi
+
+if grep -qE "i386|x86_64" "$CURDIR/Makefile"; then
+function update() {
+	local type="$1"
+	local repo="$2"
+	local res="$3"
+	local tag sha old_sha line
+
+	tag="$(curl -H "Authorization: $GITHUB_TOKEN" -sL "https://api.github.com/repos/$repo/releases/latest" | jq -r ".tag_name")"
+	[ -n "$tag" ] && {
+	sha="$(curl -sL https://github.com/$repo/releases/download/$tag/chinadns-ng@${type}@${res}@fast+lto | sha256sum | awk '{print $1}')"
+
+	[ -n "$sha" ] || return 1
+
+	line="$(awk "/PKG_ARCH:=chinadns-ng@${type}@${res}@fast\+lto/ {print NR}" "$CURDIR/Makefile")"
+
+	old_sha="$(awk -F "PKG_HASH:=" -v next_line="$((line + 1))" 'NR==next_line {print $2}' "$CURDIR/Makefile" | xargs)"
+
+	[ "$sha" != "$old_sha" ] || return 2	
+
+	sed -i -e "$((line + 1))s/PKG_HASH:=.*/PKG_HASH:=$sha/" "$CURDIR/Makefile"
+	}
+}
+
+update "i386-linux-musl" "zfl9/chinadns-ng" "i686"
+update "x86_64-linux-musl" "zfl9/chinadns-ng" "x86_64"
 fi
