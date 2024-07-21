895c02dbb80d43a5c3b00edb302de7f7d0425a05
zxlhhyccc <zxlhhyccc@gmail.com>
2024-07-21 23:30:44 +0800

    chinadns-ng: update to 2024.07.21

--- ctcgfw/openwrt-chinadns-ng/update.sh
+++ ctcgfw/openwrt-chinadns-ng/update.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 function urldecode() { : "${*//+/ }"; echo -e "${_//%/\\x}"; }
 
 export CURDIR="$(cd "$(dirname $0)"; pwd)"
@@ -15,22 +17,23 @@ for i in $(jq -r '.assets[].browser_download_url | select(contains("chinadns-ng%
 	i="$(urldecode "$i")"
 
 	arch="$(awk -F '@' '{printf "%s@%s", $2, $3}' <<< "$i")"
-	line="$(sed -n "/.*wolfssl@$arch@/=" "$CURDIR/Makefile")"
+   
+	line="$(sed -n "/PKG_ARCH:=.*wolfssl@$arch@/=" "$CURDIR/Makefile")"
 	[ -n "$line" ] || continue
-
-	sha256="$(curl -fsSL "$i" | sha256sum | awk '{print $1}')" || exit 1
-	sed -i "$((line + 1))s/PKG_HASH:=.*/PKG_HASH:=$sha256/" "$CURDIR/Makefile"
+        sha256="$(curl -fsSL "$i" | sha256sum | awk '{print $1}')" || exit 1
+        sed -i "$((line + 1))s/PKG_HASH:=.*/PKG_HASH:=$sha256/" "$CURDIR/Makefile"
 done
 
-for url in $(jq -r '.assets[].browser_download_url | select(contains("chinadns-ng%2Bwolfssl_noasm%40"))' <<< "$TAG_INFO"); do
-	decoded_url="$(urldecode "$url")"
+for i in $(jq -r '.assets[].browser_download_url | select(contains("chinadns-ng%2Bwolfssl_noasm%40"))' <<< "$TAG_INFO"); do
+	i="$(urldecode "$i")"
 
-	arch="$(awk -F '@' '{printf "%s@%s", $2, $3}' <<< "$decoded_url")"
-	line="$(sed -n "/.*noasm@$arch@/=" "$CURDIR/Makefile")"
+	arch="$(awk -F '@' '{printf "%s@%s", $2, $3}' <<< "$i")"
+	line="$(sed -n "/PKG_ARCH:=.*noasm@$arch/=" "$CURDIR/Makefile")"
 	[ -n "$line" ] || continue
 
-	sha256="$(curl -fsSL "$decoded_url" | sha256sum | awk '{print $1}')" || exit 1
-	sed -i "$((line + 1))s/PKG_HASH:=.*/PKG_HASH:=$sha256/" "$CURDIR/Makefile"
+        sha256="$(curl -fsSL "$i" | sha256sum | awk '{print $1}')" || exit 1
+        sed -i "$((line + 1))s/PKG_HASH:=.*/PKG_HASH:=$sha256/" "$CURDIR/Makefile"
 done
 
 sed -i "s,PKG_VERSION:=.*,PKG_VERSION:=$VERSION,g" "$CURDIR/Makefile"
+
