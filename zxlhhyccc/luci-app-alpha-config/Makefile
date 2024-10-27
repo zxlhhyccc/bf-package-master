d12b1042039c6dd0f47adbf24439e7645922aa1e
zxlhhyccc <zxlhhyccc@gmail.com>
2024-10-27 20:32:16 +0800

    Update luci-app-alpha-config.json/alpha-config.js

--- zxlhhyccc/luci-app-alpha-config/Makefile
+++ zxlhhyccc/luci-app-alpha-config/Makefile
@@ -8,7 +8,7 @@ LUCI_TITLE:=LuCI for Alpha theme configuration
 LUCI_DESCRIPTION:=Configuration Alpha theme in LuCI.
 
 PKG_MAINTAINER:=Hilman Maulana <hilman0.0maulana@gmail.com>
-PKG_VERSION:=1.4
+PKG_VERSION:=2.1
 PKG_LICENSE:=Apache-2.0
 
 include $(TOPDIR)/feeds/luci/luci.mk
--- zxlhhyccc/luci-app-alpha-config/htdocs/luci-static/resources/view/alpha-config.js
+++ zxlhhyccc/luci-app-alpha-config/htdocs/luci-static/resources/view/alpha-config.js
@@ -12,128 +12,111 @@
 return view.extend({
 	render: function () {
 		var m, s, o;
-		m = new form.Map('alpha' , _('Alpha theme configuration'), _('Here you can set background login and dashboard themes. Chrome is recommended.'));
+		m = new form.Map('alpha', _('Alpha theme configuration'), _('Here you can set background login and dashboard themes. Chrome is recommended.'));
 
-		s = m.section(form.TypedSection, null , _('Theme configuration'));
+		s = m.section(form.TypedSection, 'theme', _('Theme configuration'));
 		s.anonymous = true;
-
-		o = s.option(form.Value, 'color', _('Primary Color'), _('A HEX color (default: #2222359a).'))
+		o = s.option(form.Value, 'color', _('Primary color'), _('A HEX color (default: #2222359a).'));
 		o.rmempty = false;
 		o.validate = function(section_id, value) {
 			if (section_id)
-				return /^#([0-9a-fA-F]{6}|[0-9a-fA-F]{8}|[0-9a-fA-F]{3}|[0-9a-fA-F]{4})$/i.test(value) ||
-					_('Expecting: %s').format(_('valid HEX color value'));
+				return /^#([0-9a-fA-F]{6}|[0-9a-fA-F]{8}|[0-9a-fA-F]{3}|[0-9a-fA-F]{4})$/i.test(value) || _('Expecting: valid HEX color value');
 			return true;
-		}
-
-		o = s.option(form.ListValue, 'blur', _('Transparency level'), _('Transparent level for menu'));
-		o.value('0', _('0'));
+		};
+		o = s.option(form.ListValue, 'blur', _('Transparency level'), _('Transparent level for menu.'));
+		o.value('00', _('0'));
 		o.value('10', _('1'));
 		o.value('20', _('2'));
 		o.value('30', _('3'));
 		o.value('40', _('4'));
 		o.value('50', _('5'));
 		o.rmempty = false;
-
-		s = m.section(form.TypedSection, null , _('Navigation bar configuration'));
-		s.anonymous = true;
-
-		o = s.option(form.ListValue, 'nav_01', _('Navigation line 01'));
-		o.value('/cgi-bin/luci/admin/modem/main', _('Modem'));
-		o.value('/cgi-bin/luci/admin/services/neko', _('Neko'));
-		o.value('/cgi-bin/luci/admin/network/network', _('Network'));
-		o.value('/cgi-bin/luci/admin/services/openclash', _('Open Clash'));
-		o.value('/cgi-bin/luci/admin/status/overview', _('Overview'));
-		o.value('/cgi-bin/luci/admin/services/ttyd', _('Terminal'));
-		o.value('/cgi-bin/luci/admin/nas/tinyfm', _('Tiny File Manager'));
-		o.value('none', _('None'));
-		o.rmempty = false;
-
-		o = s.option(form.ListValue, 'nav_02', _('Navigation line 02'));
-		o.value('/cgi-bin/luci/admin/modem/main', _('Modem'));
-		o.value('/cgi-bin/luci/admin/services/neko', _('Neko'));
-		o.value('/cgi-bin/luci/admin/network/network', _('Network'));
-		o.value('/cgi-bin/luci/admin/services/openclash', _('Open Clash'));
-		o.value('/cgi-bin/luci/admin/status/overview', _('Overview'));
-		o.value('/cgi-bin/luci/admin/services/ttyd', _('Terminal'));
-		o.value('/cgi-bin/luci/admin/nas/tinyfm', _('Tiny File Manager'));
-		o.value('none', _('None'));
-		o.rmempty = false;
-
-		o = s.option(form.ListValue, 'nav_03', _('Navigation line 03'));
-		o.value('/cgi-bin/luci/admin/modem/main', _('Modem'));
-		o.value('/cgi-bin/luci/admin/services/neko', _('Neko'));
-		o.value('/cgi-bin/luci/admin/network/network', _('Network'));
-		o.value('/cgi-bin/luci/admin/services/openclash', _('Open Clash'));
-		o.value('/cgi-bin/luci/admin/status/overview', _('Overview'));
-		o.value('/cgi-bin/luci/admin/services/ttyd', _('Terminal'));
-		o.value('/cgi-bin/luci/admin/nas/tinyfm', _('Tiny File Manager'));
-		o.value('none', _('None'));
-		o.rmempty = false;
-
-		o = s.option(form.ListValue, 'nav_04', _('Navigation line 04'));
-		o.value('/cgi-bin/luci/admin/modem/main', _('Modem'));
-		o.value('/cgi-bin/luci/admin/services/neko', _('Neko'));
-		o.value('/cgi-bin/luci/admin/network/network', _('Network'));
-		o.value('/cgi-bin/luci/admin/services/openclash', _('Open Clash'));
-		o.value('/cgi-bin/luci/admin/status/overview', _('Overview'));
-		o.value('/cgi-bin/luci/admin/services/ttyd', _('Terminal'));
-		o.value('/cgi-bin/luci/admin/nas/tinyfm', _('Tiny File Manager'));
-		o.value('none', _('None'));
-		o.rmempty = false;
-
-		o = s.option(form.ListValue, 'nav_05', _('Navigation line 05'));
-		o.value('/cgi-bin/luci/admin/modem/main', _('Modem'));
-		o.value('/cgi-bin/luci/admin/services/neko', _('Neko'));
-		o.value('/cgi-bin/luci/admin/network/network', _('Network'));
-		o.value('/cgi-bin/luci/admin/services/openclash', _('Open Clash'));
-		o.value('/cgi-bin/luci/admin/status/overview', _('Overview'));
-		o.value('/cgi-bin/luci/admin/services/ttyd', _('Terminal'));
-		o.value('/cgi-bin/luci/admin/nas/tinyfm', _('Tiny File Manager'));
-		o.value('none', _('None'));
-		o.rmempty = false;
-
-		o = s.option(form.ListValue, 'nav_06', _('Navigation line 06'));
-		o.value('/cgi-bin/luci/admin/modem/main', _('Modem'));
-		o.value('/cgi-bin/luci/admin/services/neko', _('Neko'));
-		o.value('/cgi-bin/luci/admin/network/network', _('Network'));
-		o.value('/cgi-bin/luci/admin/services/openclash', _('Open Clash'));
-		o.value('/cgi-bin/luci/admin/status/overview', _('Overview'));
-		o.value('/cgi-bin/luci/admin/services/ttyd', _('Terminal'));
-		o.value('/cgi-bin/luci/admin/nas/tinyfm', _('Tiny File Manager'));
-		o.value('none', _('None'));
+		o = s.option(form.Flag, 'navbar', _('Navigation bar'), _('Enable navigation bar menu.'));
 		o.rmempty = false;
 
 		var bg_path = '/www/luci-static/alpha/background/';
-		s = m.section(form.TypedSection, null , _('Background configuration'), _('You can upload files such as jpg or png files, and files will be uploaded to <code>%s</code>.').format(bg_path));
+		s = m.section(form.TypedSection, 'theme' , _('Background configuration'), _('You can upload files such as jpg or png files, and files will be uploaded to <code>%s</code>.').format(bg_path));
 		s.anonymous = true;
-
-		o = s.option(form.Button, 'login', _('Login'), _('Upload file for login background'));
+		o = s.option(form.Button, 'login', _('Login'), _('Upload file for login background.'));
 		o.inputstyle = 'action';
 		o.inputtitle = _('Upload');
-		o.onclick = function(ev, section_id) {
+		o.onclick = function(ev) {
 			var file = bg_path + 'login.png';
-			return ui.uploadFile(file, ev.target).then(function(res) {
-				return fs.exec('/bin/chmod', ['0644', file]).then(function() {
+			return ui.uploadFile(file, ev.target).then(function() {
+				return fs.exec('chmod', ['777', file]).then(function() {
 					ui.addNotification(null, E('p', _('Login picture successfully uploaded.')));
 				});
-			})
-			.catch(function(e) { ui.addNotification(null, E('p', e.message)); });
+			}).catch(function(e) { ui.addNotification(null, E('p', e.message)); });
+		};
+		o.modalonly = true;
+		o = s.option(form.Button, 'dashboard', _('Dashboard'), _('Upload file for dashboard background.'));
+		o.inputstyle = 'action';
+		o.inputtitle = _('Upload');
+		o.onclick = function(ev) {
+			var file = bg_path + 'dashboard.png';
+			return ui.uploadFile(file, ev.target).then(function() {
+				return fs.exec('chmod', ['777', file]).then(function() {
+					ui.addNotification(null, E('p', _('Dashboard picture successfully uploaded.')));
+				});
+			}).catch(function(e) { ui.addNotification(null, E('p', e.message)); });
 		};
 		o.modalonly = true;
 
-		o = s.option(form.Button, 'dashboard', _('Dashboard'), _('Upload file for dashboard background'));
-		o.inputstyle = 'action';
-		o.inputtitle = _('Upload');
-		o.onclick = function(ev, section_id) {
-			var file = bg_path + 'dashboard.png';
-			return ui.uploadFile(file, ev.target).then(function(res) {
-				return fs.exec('/bin/chmod', ['0644', file]).then(function() {
-					ui.addNotification(null, E('p', _('Dashboard picture successfully uploaded.')));
-				});
-			})
-			.catch(function(e) { ui.addNotification(null, E('p', e.message)); });
+		s = m.section(form.GridSection, 'navbar', _('Navigation bar configuration'));
+		s.anonymous = true;
+		s.addremove = true;
+		s.rowcolors = true;
+		s.modaltitle = _('Add new navigation')
+		s.addbtntitle = _('Add new navigation...');
+		o = s.option(form.DummyValue, 'name', _('Name'));
+		o.modalonly = false;
+		o = s.option(form.DummyValue, 'enable', _('Status'));
+		o.modalonly = false;
+		o = s.option(form.DummyValue, 'line', _('Line'));
+		o.modalonly = false;
+		o = s.option(form.DummyValue, 'newtab', _('New tab'));
+		o.modalonly = false;
+		o = s.option(form.DummyValue, 'icon', _('Icon'));
+		o.modalonly = false;
+		o = s.option(form.DummyValue, 'address', _('Address'));
+		o.modalonly = false;
+
+		o = s.option(form.Value, 'name', _('Name navigation bar'));
+		o.rmempty = false;
+		o.modalonly = true;
+		o = s.option(form.Flag, 'enable', _('Navigation bar'), _('Enable navigation bar.'));
+		o.rmempty = false;
+		o.modalonly = true;
+		o.enabled = _('Enable');
+		o.disabled = _('Disable');
+		o.default = o.enabled;
+		o = s.option(form.Value, 'line', _('Line number'), _('Enter a line number between 1 to 10.'));
+		o.rmempty = false;
+		o.modalonly = true;
+		o.datatype = 'range(1,10)';
+		o.placeholder = '1-10';
+		o = s.option(form.Flag, 'newtab', _('Open in new tab'), _('Enable open links in a new tab.'));
+		o.rmempty = false;
+		o.modalonly = true;
+		o.enabled = _('Yes');
+		o.disabled = _('No');
+		o = s.option(form.FileUpload, 'icon', _('Icon'), _('Upload PNG file with size 256x256.'));
+		o.rmempty = false;
+		o.modalonly = true;
+		o.datatype = 'png';
+		o.root_directory = '/www/luci-static/alpha/gaya/icon/navbar';
+		o.onchange = function(ev, section_id, value) {
+			fs.exec('chmod', ['777', value]);
+			return true;
 		};
+		o = s.option(form.Value, 'address', _('Address'));
+		o.value('/cgi-bin/luci/admin/modem/main', _('Modem'));
+		o.value('/cgi-bin/luci/admin/services/neko', _('Neko'));
+		o.value('/cgi-bin/luci/admin/network/network', _('Network'));
+		o.value('/cgi-bin/luci/admin/services/openclash', _('Open Clash'));
+		o.value('/cgi-bin/luci/admin/status/overview', _('Overview'));
+		o.value('/cgi-bin/luci/admin/services/ttyd', _('Terminal'));
+		o.rmempty = false;
 		o.modalonly = true;
 
 		return m.render();
