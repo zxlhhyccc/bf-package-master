5eb48ff44a6d1ffd878330279989e1e4459e601d
zxlhhyccc <zxlhhyccc@gmail.com>
2025-02-04 23:14:30 +0800

    luci-app-zerotier-update2: Update fw3

--- zxlhhyccc/luci-app-zerotier-update2/ztnat.sh
+++ zxlhhyccc/luci-app-zerotier-update2/ztnat.sh
@@ -11,12 +11,11 @@ is_fw4=$(command -v fw4 >/dev/null && echo "1" || echo "0")
 if [ "$is_fw4" -eq "1" ]; then
     nft_incdir="/usr/share/nftables.d/chain-pre"
     rm -f "$nft_incdir/input/zerotier.nft" "$nft_incdir/forward/zerotier.nft" "$nft_incdir/srcnat/zerotier.nft"
-
-    [ -d "$nft_incdir/input" ] || mkdir -p "$nft_incdir/input"
+    mkdir -p "$nft_incdir/input" "$nft_incdir/forward" "$nft_incdir/srcnat"
 
     primaryPort=$(zerotier-cli -j info 2>/dev/null | jsonfilter -q -e '@.config.settings.primaryPort')
     if [ -n "$primaryPort" ]; then
-        echo "udp dport $primaryPort counter accept comment \"!fw4: Allow-ZeroTier-Inbound(primaryPort)\"" >> "$nft_incdir/input/zerotier.nft"
+        echo "udp dport $primaryPort counter accept comment \"!fw4: Allow-ZeroTier-Inbound(primaryPort)\"" > "$nft_incdir/input/zerotier.nft"
         logger -t "zerotier" "primaryPort $primaryPort rules added!"
     fi
 
@@ -29,24 +28,24 @@ else
     primaryPort=$(zerotier-cli -j info 2>/dev/null | jsonfilter -q -e '@.config.settings.primaryPort')
     if [ -n "$primaryPort" ]; then
         uci -q delete firewall.zerotier_in_primary
-        uci -q set firewall.zerotier_in_primary="rule"
-        uci -q set firewall.zerotier_in_primary.name="Allow-ZeroTier-Inbound(primaryPort)"
-        uci -q set firewall.zerotier_in_primary.src="wan"
-        uci -q set firewall.zerotier_in_primary.proto="udp"
-        uci -q set firewall.zerotier_in_primary.dest_port="$primaryPort"
-        uci -q set firewall.zerotier_in_primary.target="ACCEPT"
+        uci set firewall.zerotier_in_primary="rule"
+        uci set firewall.zerotier_in_primary.name="Allow-ZeroTier-Inbound(primaryPort)"
+        uci set firewall.zerotier_in_primary.src="wan"
+        uci set firewall.zerotier_in_primary.proto="udp"
+        uci set firewall.zerotier_in_primary.dest_port="$primaryPort"
+        uci set firewall.zerotier_in_primary.target="ACCEPT"
         logger -t "zerotier" "primaryPort $primaryPort rules added!"
     fi
 
     secondaryPort=$(zerotier-cli -j info 2>/dev/null | jsonfilter -q -e '@.config.settings.secondaryPort')
     if [ -n "$secondaryPort" ]; then
         uci -q delete firewall.zerotier_in_secondary
-        uci -q set firewall.zerotier_in_secondary="rule"
-        uci -q set firewall.zerotier_in_secondary.name="Allow-ZeroTier-Inbound(secondaryPort)"
-        uci -q set firewall.zerotier_in_secondary.src="wan"
-        uci -q set firewall.zerotier_in_secondary.proto="udp"
-        uci -q set firewall.zerotier_in_secondary.dest_port="$secondaryPort"
-        uci -q set firewall.zerotier_in_secondary.target="ACCEPT"
+        uci set firewall.zerotier_in_secondary="rule"
+        uci set firewall.zerotier_in_secondary.name="Allow-ZeroTier-Inbound(secondaryPort)"
+        uci set firewall.zerotier_in_secondary.src="wan"
+        uci set firewall.zerotier_in_secondary.proto="udp"
+        uci set firewall.zerotier_in_secondary.dest_port="$secondaryPort"
+        uci set firewall.zerotier_in_secondary.target="ACCEPT"
         logger -t "zerotier" "secondaryPort $secondaryPort rules added!"
     fi
 fi
@@ -57,16 +56,12 @@ zerotier_nat() {
     local portDeviceName ip_segment
     config_get id "$cfg" 'id'
     config_get_bool auto_nat "$cfg" 'auto_nat' 0
-    echo "id $id"
-    if [ -n "$id" -a "$auto_nat" -eq "1" ]; then
+
+    if [ -n "$id" ] && [ "$auto_nat" -eq "1" ]; then
         portDeviceName=$(zerotier-cli -j listnetworks 2>/dev/null | jsonfilter -q -e "@[@.nwid=\"$id\"].portDeviceName")
         [ -n "$portDeviceName" ] || return 0
-        echo "$portDeviceName"
 
         if [ "$is_fw4" -eq "1" ]; then
-            [ -d "$nft_incdir/input" ] || mkdir -p "$nft_incdir/input"
-            [ -d "$nft_incdir/forward" ] || mkdir -p "$nft_incdir/forward"
-            [ -d "$nft_incdir/srcnat" ] || mkdir -p "$nft_incdir/srcnat"
             ip_segment="$(ip route | grep "dev $portDeviceName proto kernel" | awk '{print $1}')"
             echo "iifname $portDeviceName counter accept comment \"!fw4: Zerotier allow inbound $portDeviceName\"" >> "$nft_incdir/input/zerotier.nft"
             echo "iifname $portDeviceName counter accept comment \"!fw4: Zerotier allow inbound forward $portDeviceName\"" >> "$nft_incdir/forward/zerotier.nft"
@@ -76,27 +71,27 @@ zerotier_nat() {
         else
             ip_segment="$(ip route | grep "dev $portDeviceName proto kernel" | awk '{print $1}')"
             uci -q delete firewall.zerotier_forward
-            uci -q set firewall.zerotier_forward="zone"
-            uci -q set firewall.zerotier_forward.name="zerotier"
-            uci -q set firewall.zerotier_forward.input="ACCEPT"
-            uci -q set firewall.zerotier_forward.forward="ACCEPT"
-            uci -q set firewall.zerotier_forward.output="ACCEPT"
-            uci -q set firewall.zerotier_forward.network="$portDeviceName"
-            uci -q set firewall.zerotier_forward.masq="1"
-            [ -n "$ip_segment" ] && uci -q set firewall.zerotier_forward.subnet="$ip_segment"
+            uci set firewall.zerotier_forward="zone"
+            uci set firewall.zerotier_forward.name="zerotier"
+            uci set firewall.zerotier_forward.input="ACCEPT"
+            uci set firewall.zerotier_forward.forward="ACCEPT"
+            uci set firewall.zerotier_forward.output="ACCEPT"
+            uci set firewall.zerotier_forward.network="$portDeviceName"
+            uci set firewall.zerotier_forward.masq="1"
+            [ -n "$ip_segment" ] && uci set firewall.zerotier_forward.subnet="$ip_segment"
         fi
         logger -t "zerotier" "interface $id auto nat rules added!"
     fi
 }
+
 config_load zerotier
 config_foreach zerotier_nat 'network'
 
 if [ "$is_fw4" -eq "1" ]; then
     uci -q set firewall.@defaults[0].auto_includes="1"
-    uci -q commit firewall
+    uci commit firewall
     fw4 reload
 else
-    uci -q set firewall.@defaults[0].auto_includes="1"
     uci commit firewall
     /etc/init.d/firewall reload
 fi
