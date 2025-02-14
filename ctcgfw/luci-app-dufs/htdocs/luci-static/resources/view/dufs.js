6c8b2e9e59a5d585259e4f86891e8f4fa9573335
zxlhhyccc <zxlhhyccc@gmail.com>
2025-02-14 21:24:01 +0800

    luci-app-dufs: support (URL) path_prefix

--- ctcgfw/luci-app-dufs/htdocs/luci-static/resources/view/dufs.js
+++ ctcgfw/luci-app-dufs/htdocs/luci-static/resources/view/dufs.js
@@ -5,7 +5,7 @@
 'require uci';
 'require view';
 
-const callServiceList = rpc.declare({
+var callServiceList = rpc.declare({
 	object: 'service',
 	method: 'list',
 	params: ['name'],
@@ -22,12 +22,13 @@ function getServiceStatus() {
 	});
 }
 
-function renderStatus(isRunning, port) {
+function renderStatus(isRunning, port, path) {
 	var spanTemp = '<em><span style="color:%s"><strong>%s %s</strong></span></em>';
 	var renderHTML;
 	if (isRunning) {
-		var button = String.format('&#160;<a class="btn cbi-button" href="http://%s:%s" target="_blank" rel="noreferrer noopener">%s</a>',
-			window.location.hostname, port, _('Open Web Interface'));
+		var dufsURL = '//' + window.location.hostname + ':' + port + '/' + path.replace(/^\//, '');
+		var button = String.format('&#160;<a class="btn cbi-button" href="%s" target="_blank" rel="noreferrer noopener">%s</a>',
+			dufsURL, _('Open Web Interface'));
 		renderHTML = spanTemp.format('green', _('Dufs'), _('RUNNING')) + button;
 	} else {
 		renderHTML = spanTemp.format('red', _('Dufs'), _('NOT RUNNING'));
@@ -44,8 +45,9 @@ return view.extend({
 	},
 
 	render: function(data) {
-		let m, s, o;
+		var m, s, o;
 		var webport = uci.get(data[0], 'config', 'port') || '5244';
+		var webpath = uci.get(data[0], 'config', 'path_prefix') || '/';
 
 		m = new form.Map('dufs', _('Dufs'),
 			_('Dufs is a distinctive utility file server that supports static serving, uploading, searching, accessing control, webdav...'));
@@ -56,12 +58,12 @@ return view.extend({
 			poll.add(function () {
 				return L.resolveDefault(getServiceStatus()).then(function (res) {
 					var view = document.getElementById('service_status');
-					view.innerHTML = renderStatus(res, webport);
+					view.innerHTML = renderStatus(res, webport, webpath);
 				});
 			});
 
 			return E('div', { class: 'cbi-section', id: 'status_bar' }, [
-					E('p', { id: 'service_status' }, _('Collecting data…'))
+				E('p', { id: 'service_status' }, _('Collecting data…'))
 			]);
 		}
 
@@ -78,11 +80,9 @@ return view.extend({
 		o.datatype = 'port';
 		o.placeholder = '5000';
 
-		o = s.option(form.Value, 'tls_cert', _('Certificate pubkey path'),
-			_('Path to an SSL/TLS certificate to serve with HTTPS.'));
-
-		o = s.option(form.Value, 'tls_key', _('Certificate privkey path'),
-			_('Path to the SSL/TLS certificate\'s private key.'));
+		o = s.option(form.Value, 'path_prefix', _('Path Prefix'),
+			_('Specify a (URL) path prefix'));
+		o.placeholder = 'dufs';
 
 		o = s.option(form.Flag, 'enable_cors', _('Enable CORS'));
 
