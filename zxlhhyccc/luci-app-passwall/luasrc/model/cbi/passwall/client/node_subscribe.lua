66f06c35abf4c38001e4f014233f79b94d427343
zxlhhyccc <zxlhhyccc@gmail.com>
2025-07-21 22:51:39 +0800

    luci-app-passwall: luci: bump to 25.7.15

--- zxlhhyccc/luci-app-passwall/luasrc/model/cbi/passwall/client/node_subscribe.lua
+++ zxlhhyccc/luci-app-passwall/luasrc/model/cbi/passwall/client/node_subscribe.lua
@@ -66,6 +66,11 @@ if api.is_js_luci() then
 	end
 end
 
+m.render = function(self, ...)
+	Map.render(self, ...)
+	api.optimize_cbi_ui()
+end
+
 -- [[ Subscribe Settings ]]--
 s = m:section(TypedSection, "global_subscribe", "")
 s.anonymous = true
@@ -145,7 +150,7 @@ end
 o = s:option(Button, "_update", translate("Manual subscription All"))
 o.inputstyle = "apply"
 function o.write(t, n)
-	luci.sys.call("lua /usr/share/" .. appname .. "/subscribe.lua start > /dev/null 2>&1 &")
+	luci.sys.call("lua /usr/share/" .. appname .. "/subscribe.lua start all manual > /dev/null 2>&1 &")
 	m.no_commit = true
 	luci.http.redirect(api.url("log"))
 end
@@ -215,7 +220,7 @@ end
 o = s:option(Button, "_update", translate("Manual subscription"))
 o.inputstyle = "apply"
 function o.write(t, n)
-	luci.sys.call("lua /usr/share/" .. appname .. "/subscribe.lua start " .. n .. " > /dev/null 2>&1 &")
+	luci.sys.call("lua /usr/share/" .. appname .. "/subscribe.lua start " .. n .. " manual > /dev/null 2>&1 &")
 	m.no_commit = true
 	luci.http.redirect(api.url("log"))
 end
