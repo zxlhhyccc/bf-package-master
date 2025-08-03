ad803db56464773c602c3b6bb465458822199d1a
zxlhhyccc <zxlhhyccc@gmail.com>
2025-08-03 20:43:36 +0800

    openclash-meta: Add update script

--- zxlhhyccc/openclash-meta/package-update.sh
+++ zxlhhyccc/openclash-meta/package-update.sh
@@ -7,31 +7,28 @@ pushd ~/ax6-6.6 || exit 1
 
 export CURDIR="$(cd "$(dirname $0)"; pwd)"
 
-OLD_VER=$(grep -oP '^PKG_VERSION:=\K.*' "$CURDIR/Makefile")
-OLD_COMMIT=$(grep -oP '^PKG_SOURCE_VERSION:=\K.*' "$CURDIR/Makefile")
+OLD_COMMIT_FULL=$(grep -oP '^PKG_SOURCE_VERSION:=\K.*' "$CURDIR/Makefile")
+OLD_COMMIT=${OLD_COMMIT_FULL:0:8}
+
 OLD_CHECKSUM=$(grep -oP '^PKG_MIRROR_HASH:=\K.*' "$CURDIR/Makefile")
 
-REPO="https://github.com/fatedier/frp"
-REPO_API="https://api.github.com/repos/fatedier/frp/releases/latest"
+REPO="https://github.com/MetaCubeX/mihomo"
 
-# 获取新 TAG、COMMIT 等
-TAG="$(curl -H "Authorization: $GITHUB_TOKEN" -sL "$REPO_API" | jq -r ".tag_name")"
-COMMIT="$(git ls-remote "$REPO" "refs/heads/master" | cut -f1)"
-VER="${TAG#v}"  # TAG 形如 v1.8.11
+# 获取新 COMMIT
+COMMIT="$(git ls-remote "$REPO" "refs/heads/Alpha" | cut -f1)"
 
-# 如果版本或 commit 变了，才清除并更新
-if [ "$VER" != "$OLD_VER" ] || [ "$COMMIT" != "$OLD_COMMIT" ]; then
-    echo "新版本: $VER / $COMMIT，旧版本: $OLD_VER / $OLD_COMMIT"
+# 如果 commit 变了，才清除并更新
+if [ "$COMMIT" != "$OLD_COMMIT_FULL" ]; then
+    echo "新版本: $COMMIT，旧版本: $OLD_COMMIT_FULL"
 
     # 删除旧源码包和哈希
-    rm -f dl/frp-${OLD_VER}.tar.gz
+    rm -f dl/openclash-meta-alpha-${OLD_COMMIT}.tar.zst
 
     # 清理旧缓存（触发重新编译）
-    make package/frp/clean V=s
+    make package/openclash-meta/clean V=s
 
     # 修改 Makefile 中的版本和提交哈希
     ./staging_dir/host/bin/sed -i "$CURDIR/Makefile" \
-        -e "s|^PKG_VERSION:=.*|PKG_VERSION:=${VER}|" \
         -e "s|^PKG_SOURCE_VERSION:=.*|PKG_SOURCE_VERSION:=${COMMIT}|"
 
     echo "🧹 清空旧 HASH：$OLD_CHECKSUM"
@@ -39,10 +36,10 @@ if [ "$VER" != "$OLD_VER" ] || [ "$COMMIT" != "$OLD_COMMIT" ]; then
         -e "s|^PKG_MIRROR_HASH:=.*|PKG_MIRROR_HASH:=|"
 
     # 重新下载源码包
-    make package/frp/download V=s
+    make package/openclash-meta/download V=s
 
     # 重新生成校验和
-    TARFILE="dl/frp-${VER}.tar.gz"
+    TARFILE="dl/openclash-meta-alpha-${OLD_COMMIT}.tar.zst"
     if [ -f "$TARFILE" ]; then
         CHECKSUM=$(./staging_dir/host/bin/mkhash sha256 "$TARFILE")
         ./staging_dir/host/bin/sed -i "$CURDIR/Makefile" \
