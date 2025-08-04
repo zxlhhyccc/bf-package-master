4a56084fb148241c07e949722d542976b3c844a3
zxlhhyccc <zxlhhyccc@gmail.com>
2025-08-04 23:35:42 +0800

    smartdns: Update to git HEAD and Add update script

--- ntlf9t/smartdns/package-update.sh
+++ ntlf9t/smartdns/package-update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# 自动更新 kcptun 版本、commit 并计算 HASH
+# 自动更新 smartdns 版本、commit 并计算 HASH
 
 set -e
 
@@ -11,38 +11,33 @@ OLD_VER=$(grep -oP '^PKG_VERSION:=\K.*' "$CURDIR/Makefile")
 OLD_COMMIT=$(grep -oP '^PKG_SOURCE_VERSION:=\K.*' "$CURDIR/Makefile")
 OLD_CHECKSUM=$(grep -oP '^PKG_MIRROR_HASH:=\K.*' "$CURDIR/Makefile")
 
-REPO="https://github.com/xtaci/kcptun"
-REPO_API="https://api.github.com/repos/xtaci/kcptun/releases/latest"
+REPO="https://github.com/pymumu/smartdns"
 
-# 获取新 TAG、COMMIT 等
-TAG="$(curl -H "Authorization: $GITHUB_TOKEN" -sL "$REPO_API" | jq -r ".tag_name")"
-VER="${TAG#v}"  # TAG 形如 v1.8.11
-
+# 获取新 COMMIT
 COMMIT="$(git ls-remote "$REPO" HEAD | cut -f1)"
 
 # 如果版本或 commit 变了，才清除并更新
-if [ "$VER" != "$OLD_VER" ] || [ "$COMMIT" != "$OLD_COMMIT" ]; then
-    echo "⬆️  新版本: $VER / $COMMIT，旧版本: $OLD_VER / $OLD_COMMIT"
+if [ "$COMMIT" != "$OLD_COMMIT" ]; then
+    echo "⬆️  新版本: $COMMIT，旧版本: $OLD_COMMIT"
 
     # 删除旧源码包和哈希
-    rm -f dl/kcptun-${OLD_VER}.tar.gz
+    rm -f dl/smartdns-${OLD_VER}.tar.zst
 
     # 清理旧缓存（触发重新编译）
     make package/kcptun/clean V=s
  
     # 修改 Makefile 中的版本和提交哈希
     ./staging_dir/host/bin/sed -i "$CURDIR/Makefile" \
-        -e "s|^PKG_VERSION:=.*|PKG_VERSION:=${VER}|" \
         -e "s|^PKG_SOURCE_VERSION:=.*|PKG_SOURCE_VERSION:=${COMMIT}|" \
         -e "s|^PKG_MIRROR_HASH:=.*|PKG_MIRROR_HASH:=|"
 
     echo "🧹 清空旧 HASH：$OLD_CHECKSUM"
 
     # 重新下载源码包
-    make package/kcptun/download V=s
+    make package/smartdns/download V=s
 
     # 重新生成校验和
-    TARFILE="dl/kcptun-${VER}.tar.gz"
+    TARFILE="dl/smartdns-${OLD_VER}.tar.zst"
     if [ -f "$TARFILE" ]; then
         CHECKSUM=$(./staging_dir/host/bin/mkhash sha256 "$TARFILE")
         ./staging_dir/host/bin/sed -i "$CURDIR/Makefile" \
