916a2f52025125cf0d6ac12330a78a2e2c1b4c13
zxlhhyccc <zxlhhyccc@gmail.com>
2026-01-06 20:59:14 +0800

    update: Meta to alpha-gb18a335

--- zxlhhyccc/openclash-meta/package-update.sh
+++ zxlhhyccc/openclash-meta/package-update.sh
@@ -9,29 +9,37 @@ pushd "$BUILD_DIR" > /dev/null || exit 1
 
 export CURDIR="$(cd "$(dirname $0)"; pwd)"
 
+OLD_VER=$(grep -oP '^PKG_BASE_VERSION:=\K.*' "$CURDIR/Makefile")
 OLD_COMMIT_FULL=$(grep -oP '^PKG_SOURCE_VERSION:=\K.*' "$CURDIR/Makefile")
 OLD_COMMIT=${OLD_COMMIT_FULL:0:8}
 
 OLD_CHECKSUM=$(grep -oP '^PKG_MIRROR_HASH:=\K.*' "$CURDIR/Makefile")
 
 REPO="https://github.com/MetaCubeX/mihomo"
+REPO_API="https://api.github.com/repos/MetaCubeX/mihomo/releases/latest"
+
+# 获取 GitHub API 返回的 tag、data 和 commit
+TAG="$(curl -H "Authorization: $GITHUB_TOKEN" -sL "$REPO_API" | jq -r ".tag_name")"
+API_VER="${TAG#v}"  # TAG 形如 v1.8.11
 
 # 获取新 COMMIT
 COMMIT="$(git ls-remote "$REPO" "refs/heads/Alpha" | cut -f1)"
 NEW_COMMIT=${COMMIT:0:8}
 
 # 如果 commit 变了，才清除并更新
-if [ "$COMMIT" != "$OLD_COMMIT_FULL" ]; then
-    echo "⬆️  新版本:: $COMMIT，旧版本: $OLD_COMMIT_FULL"
+if [ "$API_VER" != "$OLD_VER" ] || \
+    [ "$COMMIT" != "$OLD_COMMIT_FULL" ]; then
+    echo "⬆️  新版本: $API_VER / $COMMIT，旧版本: $OLD_VER / $OLD_COMMIT_FULL"
 
     # 删除旧源码包和哈希
-    rm -f dl/openclash-meta-alpha-${OLD_COMMIT}.tar.zst
+    rm -f dl/openclash-meta-${OLD_VER}~${OLD_COMMIT}.tar.zst
 
     # 清理旧缓存（触发重新编译）
     make package/openclash-meta/clean V=s
 
     # 修改 Makefile 中的版本和提交哈希
     ./staging_dir/host/bin/sed -i "$CURDIR/Makefile" \
+        -e "s|^PKG_BASE_VERSION:=.*|PKG_BASE_VERSION:=${API_VER}|" \
         -e "s|^PKG_SOURCE_VERSION:=.*|PKG_SOURCE_VERSION:=${COMMIT}|" \
         -e "s|^PKG_MIRROR_HASH:=.*|PKG_MIRROR_HASH:=|"
 
@@ -41,7 +49,7 @@ if [ "$COMMIT" != "$OLD_COMMIT_FULL" ]; then
     make package/openclash-meta/download V=s
 
     # 重新生成校验和
-    TARFILE="dl/openclash-meta-alpha-${NEW_COMMIT}.tar.zst"
+    TARFILE="dl/openclash-meta-${API_VER}~${NEW_COMMIT}.tar.zst"
     if [ -f "$TARFILE" ]; then
         CHECKSUM=$(./staging_dir/host/bin/mkhash sha256 "$TARFILE")
         ./staging_dir/host/bin/sed -i "$CURDIR/Makefile" \
