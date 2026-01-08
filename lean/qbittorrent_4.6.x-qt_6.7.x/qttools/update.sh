e6f0035994081c0c59a7eed616af8a8f23e5925c
zxlhhyccc <zxlhhyccc@gmail.com>
2026-01-08 22:36:27 +0800

    qtbase/qttools: Update scripts.

--- lean/qbittorrent_4.6.x-qt_6.7.x/qttools/update.sh
+++ lean/qbittorrent_4.6.x-qt_6.7.x/qttools/update.sh
@@ -8,9 +8,34 @@ function update() {
 	local type="$1"
 	local repo="$2"
 	local res="$3"
-	local tag sha old_hash line
+	local NEW_BASE NEW_BUGFIX sha old_hash line_base line_bugfix 
 
-	sha="$(curl -sL http://download.qt.io/official_releases/qt/${repo}/${repo}.${res}/submodules/${type}-everywhere-src-${repo}.${res}.tar.xz | sha256sum | awk '{print $1}')"
+	NEW_BASE="$(curl -fsSL https://download.qt.io/${repo}/ \
+	   | grep -o '>[0-9]\+\.[0-9]\+/' \
+	   | sed 's/[>/]//g' \
+	   | sort -V \
+	   | tail -n1)"
+
+	NEW_BUGFIX="$(curl -fsSL https://download.qt.io/${repo}/${NEW_BASE}/ \
+	   | grep -o '>[0-9]\+\.[0-9]\+\.[0-9]\+/' \
+	   | sed 's/[>/]//g' \
+	   | sort -V \
+	   | tail -n1 \
+	   | awk -F. '{print $NF}')"
+
+	[ -n "$NEW_BASE.$NEW_BUGFIX" ] || return 1
+
+	OLD_BASE="$(awk -F "PKG_BASE:=" '{print $2}' "$CURDIR/Makefile" | xargs)"
+	OLD_BUGFIX="$(awk -F "PKG_BUGFIX:=" '{print $2}' "$CURDIR/Makefile" | xargs)"
+
+	[ "$NEW_BASE.$NEW_BUGFIX" != "$OLD_BASE.$OLD_BUGFIX" ] || return 2
+
+	line_base="$(awk "/PKG_BASE:=/ {print NR}" "$CURDIR/Makefile")"
+	sed -i -e "$((line_base))s/PKG_BASE:=.*/PKG_BASE:=$NEW_BASE/" "$CURDIR/Makefile"
+	line_bugfix="$(awk "/PKG_BUGFIX:=/ {print NR}" "$CURDIR/Makefile")"
+	sed -i -e "$((line_bugfix))s/PKG_BUGFIX:=.*/PKG_BUGFIX:=$NEW_BUGFIX/" "$CURDIR/Makefile"
+
+	sha="$(curl -fsSL http://download.qt.io/${repo}/${NEW_BASE}/$NEW_BASE.$NEW_BUGFIX/submodules/${type}-everywhere-src-$NEW_BASE.$NEW_BUGFIX.tar.xz | sha256sum | awk '{print $1}')"
 	[ -n "$sha" ] || return 1
 
 	old_hash="$(awk -F "PKG_HASH:=" '{print $2}' "$CURDIR/Makefile" | xargs)"
@@ -20,4 +45,4 @@ function update() {
 	   sed -i -e "$((line))s/PKG_HASH:=.*/PKG_HASH:=$sha/" "$CURDIR/Makefile"
 }
 
-update "qttools" "$(awk -F "PKG_BASE:=" '{print $2}' "$CURDIR/Makefile" | xargs)" "$(awk -F "PKG_BUGFIX:=" '{print $2}' "$CURDIR/Makefile" | xargs)"
+update "qttools" "official_releases/qt" "$(awk -F "PKG_BASE:=" '{print $2}' "$CURDIR/Makefile" | xargs)"
