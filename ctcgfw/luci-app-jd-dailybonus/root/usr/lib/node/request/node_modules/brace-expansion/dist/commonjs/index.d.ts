ee5306a3b5eac04725f7e5de5abc9f86281a1119
zxlhhyccc <zxlhhyccc@gmail.com>
2026-07-22 20:28:40 +0800

    luci-app-jd-dailybonus: update deps.

--- ctcgfw/luci-app-jd-dailybonus/root/usr/lib/node/request/node_modules/brace-expansion/dist/commonjs/index.d.ts
+++
@@ -1,8 +0,0 @@
-export declare const EXPANSION_MAX = 100000;
-export declare const EXPANSION_MAX_LENGTH = 4000000;
-export type BraceExpansionOptions = {
-    max?: number;
-    maxLength?: number;
-};
-export declare function expand(str: string, options?: BraceExpansionOptions): string[];
-//# sourceMappingURL=index.d.ts.map
